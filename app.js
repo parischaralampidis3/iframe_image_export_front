c951b07c067a772990cd9838379a02ffdc88c709
Paris Charalampidis <parou.char@gmail.com>
2025-08-06 11:57:18 +0300

    work at parse function

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-console.log("script working");
+//console.log("script working");
+//parse function
 
-const parseFetch = await fetch("http://localhost/3000/capture/parse"),{ 
-    method: "POST",
+function parseResult(){
+fetch('http://localhost:3000/capture/parse',{
+    method:"POST",
     headers:{
-        "Content-Type":"application/json",
-    }
+        "Accept":"application/json",
+        'Content-Type':"application/json"
+    },
+    body:JSON.stringify({
+        iframe:'<iframe title="analysi_proipologizomenis_dapanis" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYzRmYmFhNjMtOTlkNi00Yjk5LTg0M2EtNGI5NjJiYjA5ZmY3IiwidCI6IjU4OTJhYWZmLTBhYTUtNGQ5YS1iNzUxLTU0NzEzZTFkMDUzYSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>'
+    })
+     })
+    .then((response)=>{
+return response.json();    
+})
+.then((data) => console.log(data))
+.catch((err)=> console.error(err))
 }
+parseResult()
+
